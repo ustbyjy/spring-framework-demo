7c839f0f0bd7e2fadbfe2862d170f5e882c7af20
yanjingyang@zulong.com <yanjingyang@zulong.com>
2018-03-09 18:44:19 +0800

    first commit

---
+++ src/test/java/com/yanwang/common/ClassPathResourceTest.java
@@ -0,0 +1,25 @@
+package com.yanwang.common;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.Test;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import java.io.IOException;
+import java.util.Date;
+
+public class ClassPathResourceTest {
+
+    @Test
+    public void read() throws IOException {
+        Resource resource = new ClassPathResource("1.txt");
+        System.out.println("filename: " + resource.getFilename());
+        System.out.println("exists: " + resource.exists());
+        System.out.println("contentLength: " + resource.contentLength());
+        System.out.println("description: " + resource.getDescription());
+        System.out.println("lastModified: " + new Date(resource.lastModified()).toLocaleString());
+        System.out.println("isReadable: " + resource.isReadable());
+        System.out.println("isOpen: " + resource.isOpen());
+        System.out.println("content: " + FileUtils.readFileToString(resource.getFile()));
+    }
+}
