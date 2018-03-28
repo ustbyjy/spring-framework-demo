a195b7d0e4f072f4337a58908d2e6a10bfd0d5fd
yanjingyang@zulong.com <yanjingyang@zulong.com>
2018-03-28 10:47:57 +0800

    SpEL

--- src/test/java/com/yjy/spel/SpELTest.java
+++ src/test/java/com/yjy/spel/SpELTest.java
@@ -1,10 +1,39 @@
 package com.yjy.spel;
 
 import org.junit.Test;
+import org.springframework.expression.Expression;
 import org.springframework.expression.ExpressionParser;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
 
 public class SpELTest {
+
+    @Test
+    public void test1() {
+        ExpressionParser parser = new SpelExpressionParser();
+        Expression exp = parser.parseExpression("'Hello World'");
+        String message = (String) exp.getValue();
+        System.out.println(message);
+
+        // concat
+        exp = parser.parseExpression("'Hello World'.concat('!')");
+        message = (String) exp.getValue();
+        System.out.println(message);
+
+        // invokes 'getBytes()'
+        exp = parser.parseExpression("'Hello World'.bytes");
+        byte[] bytes = (byte[]) exp.getValue();
+        System.out.println(new String(bytes));
+
+        // invokes 'getBytes().length'
+        exp = parser.parseExpression("'Hello World'.bytes.length");
+        int length = (Integer) exp.getValue();
+        System.out.println(length);
+
+        // Constructor
+        exp = parser.parseExpression("new String('hello world').toUpperCase()");
+        message = exp.getValue(String.class);
+        System.out.println(message);
+    }
 
     @Test
     public void testRelationalOperators() {
