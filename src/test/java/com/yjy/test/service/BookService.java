31ce3d904723e6a794b3d1190b65d0827ddc44a9
yanjingyang@zulong.com <yanjingyang@zulong.com>
2018-07-31 09:23:56 +0800

    @Resource & @Inject

--- src/test/java/com/yjy/test/service/BookService.java
+++ src/test/java/com/yjy/test/service/BookService.java
@@ -1,21 +1,30 @@
 package com.yjy.test.service;
 
 import com.yjy.test.dao.BookDao;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.inject.Inject;
+
 /**
- * {@code @Autowired}: 先按类型，后按名称
+ * {@code @Autowired}: 先按类型，后按名称，支持@Primary注解和required属性<br>
+ * {@code @Resource}: 可指定byName或者byType<br>
+ * {@code @Inject}: 和@Autowired类似，不同的是不支持required属性<br>
  */
 @Service
 public class BookService {
 
-    //    @Qualifier("bookDao")
-    @Autowired
-    private BookDao bookDao;
+//    @Qualifier("bookDao")
+//    @Autowired
+//    private BookDao bookDao;
 
 //    @Autowired
 //    private BookDao bookDao2;
+
+//    @Resource
+//    private BookDao bookDao;
+
+    @Inject
+    private BookDao bookDao;
 
     public void print() {
         System.out.println(bookDao);
